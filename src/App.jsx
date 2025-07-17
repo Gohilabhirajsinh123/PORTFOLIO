653bcbbb86e1d921edc9c25641df5a96600696b0
Gohil Abhiraj <2301031000022@silveroakuni.ac.in>
2025-07-17 12:22:29 +0530

    PORTFOLIO

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ function App() {
             {section === 'About Me' && (
               <motion.section className="section gradient-bg1" initial="hidden" animate="show" variants={fadeIn("left")} transition={{ duration: 3.5 }}>
                 <h2>About Me</h2>
-                <p>I am a passionate Full Stack Developer focused on creating impactful web experiences. I have strong foundations in both frontend and backend development, with excellent skills in team collaboration, project execution, and delivering real-world digital solutions.</p>
+                <p>Motivated B.Tech student with strong skills in HTML, CSS, JavaScript, Java, Python, and MongoDB. Seeking a Full Stack Development internship to apply my knowledge in developing responsive and scalable web applications while learning from real-world challenges.</p>
               </motion.section>
             )}
             {section === 'Skills' && (
@@ -96,16 +96,20 @@ function App() {
               <motion.section className="section gradient-bg" initial="hidden" animate="show" variants={fadeIn("right")} transition={{ duration: 8.5 }}>
                 <h2>Certifications & Courses</h2>
                 <ul>
-                  <li><a href={`${process.env.PUBLIC_URL}/Google_Cloud_Career_Launchpad_Cybersecurity_track.jpg`} target="_blank" rel="noopener noreferrer">Google Cloud Career Launchpad – Cybersecurity, May 2025</a></li>
+                  <li><a href={`${process.env.PUBLIC_URL}/Google_Cloud_Career_Launchpad_Cybersecurity_track.jpg`} target="_blank" rel="noopener noreferrer"><b>Google Cloud</b> Career Launchpad – Cybersecurity, May 2025</a></li>
                   <li><a href={`${process.env.PUBLIC_URL}/JavaScript_Training.jpg`} target="_blank" rel="noopener noreferrer">JavaScript Training – Spoken Tutorial, Mar 2025 (Score: 80%)</a></li>
-                  <li><a href={`${process.env.PUBLIC_URL}/command_line_linux.jpg`} target="_blank" rel="noopener noreferrer">Command Line in Linux – Coursera, Mar 2025</a></li>
+                  <li><a href={`${process.env.PUBLIC_URL}/command_line_linux.jpg`} target="_blank" rel="noopener noreferrer">Command Line in Linux – <b>Coursera</b>, Mar 2025</a></li>
                   <li><a href={`${process.env.PUBLIC_URL}/CSS.jpg`} target="_blank" rel="noopener noreferrer">CSS Training – Spoken Tutorial, Feb 2025 (Score: 77.5%)</a></li>
-                  <li><a href={`${process.env.PUBLIC_URL}/DBMS.jpg`} target="_blank" rel="noopener noreferrer">DBMS – Coursera, Feb 2025</a></li>
+                  <li><a href={`${process.env.PUBLIC_URL}/DBMS.jpg`} target="_blank" rel="noopener noreferrer">DBMS – <b>Coursera</b>, Feb 2025</a></li>
                   <li><a href={`${process.env.PUBLIC_URL}/HTML.jpg`} target="_blank" rel="noopener noreferrer">HTML Training – Spoken Tutorial, Sep 2024 (Score: 50%)</a></li>
-                  <li><a href={`${process.env.PUBLIC_URL}/introduction_to_bash_shellscript.jpg`} target="_blank" rel="noopener noreferrer">Intro to Bash Scripting – Coursera, Mar 2025</a></li>
+                  <li><a href={`${process.env.PUBLIC_URL}/introduction_to_bash_shellscript.jpg`} target="_blank" rel="noopener noreferrer">Intro to Bash Scripting – <b>Coursera</b>, Mar 2025</a></li>
                   <li><a href={`${process.env.PUBLIC_URL}/OSDATACAMP.jpg`} target="_blank" rel="noopener noreferrer">Intro to Bash Scripting – OSDatacamp, Mar 2025</a></li>
                   <li><a href={`${process.env.PUBLIC_URL}/JAVADATACAMP.jpg`} target="_blank" rel="noopener noreferrer">OOP in Java – DataCamp, Mar 2025</a></li>
-                  <li>Magic of Mathematics – Self Learning</li>
+                  <li>Magic of Mathematics</li>
+                  <li><a href={`${process.env.PUBLIC_URL}/AIA.jpg`} target="_blank" rel="noopener noreferrer">AI Agent – <b>IBM Skill Build</b> </a></li>
+                  <li><a href={`${process.env.PUBLIC_URL}/RMs.jpg`} target="_blank" rel="noopener noreferrer">Rise of Multiagent Systems – <b>IBM Skill Build</b></a></li>
+                  <li><a href={`${process.env.PUBLIC_URL}/UPAIA.jpg`} target="_blank" rel="noopener noreferrer">Unleashing The Power of AI Agent – <b>IBM Skill Build</b></a></li>
+                  <li><a href={`${process.env.PUBLIC_URL}/BWD.jpg`} target="_blank" rel="noopener noreferrer">Become a Web Developer – <b>IBM Skill Build</b></a></li>
                 </ul>
               </motion.section>
             )}
